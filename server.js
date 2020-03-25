a26780653a6f87666d3835453fef971b033ea831
samuel Ologunowa <samuelologunowa@gmail.com>
2020-03-25 17:51:46 +0100

    entry point

---
+++ server.js
@@ -0,0 +1,8 @@
+const { app }  = require("./index");
+const port = 3000||process.env.PORT;
+app.listen(port,(err)=>{
+    if(err){
+        console.log(err)
+    }
+    console.log("app listening on port " + port);
+});
