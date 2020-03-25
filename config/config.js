81a010eda88078f0003a6efe315524ffcba900bf
samuel Ologunowa <samuelologunowa@gmail.com>
2020-03-25 17:52:03 +0100

    config file

---
+++ config/config.js
@@ -0,0 +1,11 @@
+"use strict"
+const dotenv = require("dotenv");
+dotenv.config();
+
+module.exports = {
+    user:"sammy",
+    host:"localhost",
+    database:"hosteldb",
+    password:"sammy",
+    port:5432
+}
