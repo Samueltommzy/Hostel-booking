2f575b105038d3055632fe442827131877d16999
samuel Ologunowa <samuelologunowa@gmail.com>
2020-03-27 16:35:41 +0100

    Added more test

--- __tests__/e2e/room.spec.js
+++ __tests__/e2e/room.spec.js
@@ -9,16 +9,61 @@ describe("Test all room endpoints", function(){
             room_type:"Standard double room 2 beds",
             number_of_beds:2,
 	        price_per_bed :100000,
-	        booked_beds:[],
+	        booked_beds:[1],
 	        available_beds:[1,2],
 	        room_config_id:5
         }).then(function(res){
             expect(res.status).to.equal(201)
-        expect(res.body).to.have.property("data");
-        done();
+            expect(res.body).to.have.property("data");
+            done();
         }).catch(function(error){
             return done(error);
         })    
         
     })
+
+    it("Should get all available rooms",function(done){
+        request(app).get("/api/v1/allrooms").then(function(res){
+            expect(res.status).to.equal(200);
+            expect(res.body.data).to.be.instanceOf(Array);
+            done();
+        }).catch(function(error){
+            return done(error);
+        });
+    })
+    it("Should get a room by id"),function(done){
+        request(app).get(`/api/v1/rooms/5`).then(function(res){
+            expect(res.status).to.equal(200);
+            done();
+        }).catch(function(error){
+            return done(error);
+        })
+    }
+    it("Should get all rooms",function(done){
+        request(app).get("/api/v1/rooms").then(function(res){
+            expect(res.status).to.equal(200);
+            expect(res.body.data).to.be.instanceOf(Array);
+            done();
+        }).catch(function(error){
+            return done(error);
+        });
+    });
+    it("Should get room by type",function(done){
+        request(app).get("/api/v1/rooms/type/1").then(function(res){
+            expect(res.status).to.equal(200);
+            expect(res.body.data).to.be.instanceOf(Array);
+            done();
+        }).catch(function(error){
+            return done(error);
+        });
+    });
+    it("Should get delete a room",function(done){
+        request(app).delete("/api/v1/rooms/17").then(function(res){
+            expect(res.status).to.equal(200);
+            done();
+        }).catch(function(error){
+            return done(error);
+        });
+    })
+    
 })
